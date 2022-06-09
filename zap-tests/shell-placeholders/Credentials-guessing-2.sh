dd9f3f3d10df6eb0e849a0a7adab466321e9c84c
Riccardo ten Cate <r.tencate77@gmail.com>
2022-06-09 14:50:09 +0200

    Create Credentials-guessing-2.sh

---
+++ zap-tests/shell-placeholders/Credentials-guessing-2.sh
@@ -0,0 +1,5 @@
+python3 runners/QA.py \
+--zest_script "/Users/riccardotencate/zap-tests/zest/Credentials-guessing-2.zst"  \
+--zest_script_name "Credentials-guessing-2" \
+--zest_script_type "standalone" \
+--target "http://localhost:4444"
