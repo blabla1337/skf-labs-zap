1373083a3c567fb37d2c7e766fe24c5e207183dd
Riccardo ten Cate <riccardo.tencate@nvc3406.nedap.local>
2022-07-01 22:09:08 +0200

    AF updates

--- zap-tests/shell-placeholders/CMD.sh
+++ zap-tests/shell-placeholders/CMD.sh
@@ -1,8 +1,12 @@
-python3 runners/QA.py \
---zest_script "/Users/riccardotencate/Desktop/zap-tests/zest/CMD.zst"  \
+python3 runners/QA.py
+
+
+
+docker run -v $(pwd):/home/zap:rw -t owasp/zap2docker-stable /home/zap/QA.py \
+--zest_script "/Users/riccardo.tencate/Desktop/skf-labs-zap/zap-tests/zest/CMD.zst"  \
 --zest_script_name "CMD" \
 --zest_script_type "standalone" \
 --enable_active_scan "True" \
---active_scan_policy "/Users/riccardotencate/Desktop/zap-tests/policies/OS-COMMAND-INJECTION.policy" \
+--active_scan_policy "/Users/riccardo.tencate/Desktop/skf-labs-zap/zap-tests/policies/OS-COMMAND-INJECTION.policy" \
 --active_scan_policy_name "OS-COMMAND-INJECTION" \
---target "http://localhost:4444"
+--target "http://192.168.68.133:5000"
---
+++ zap-tests/shell-placeholders/Credentials-guessing-1.sh
@@ -0,0 +1,5 @@
+python3 runners/QA.py \
+--zest_script "/Users/riccardotencate/Desktop/zap-tests/zest/Credentials-guessing-1.zst"  \
+--zest_script_name "Credentials-guessing-1" \
+--zest_script_type "standalone" \
+--target "http://localhost:4444"
